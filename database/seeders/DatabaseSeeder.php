4cbc807d542f45e37b40565494f03723a1422603
Contradictive88 <lanceorville5@gmail.com>
2024-05-30 06:58:59 +0800

    added spatie and adjusted seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,16 +14,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create 10 users with similar format as the student user
-        User::factory()->count(10)->create();
+        // Create roles
+        $adminRole = Role::create(['name' => 'admin']);
+        $studentRole = Role::create(['name' => 'student']);
+
+        // Create 10 student users
+        $students = User::factory()->count(10)->create();
+        foreach ($students as $student) {
+            $student->assignRole($studentRole);
+        }
 
         // Create a sample admin user
-        User::create([
-            'first_name' =>'Sample',
+        $admin = User::create([
+            'first_name' => 'Sample',
             'last_name' => 'Admin',
             'username' => 'admin',
             'email' => 'admin@mail.com',
-            'password' => Hash::make('password123'), // You can hash the password using Hash::make
+            'password' => Hash::make('password123'),
         ]);
+
+        // Assign the admin role to the admin user
+        $admin->assignRole($adminRole);
     }
 }
