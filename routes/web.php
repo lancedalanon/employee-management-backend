b267b73002516d3e503b618b368f3f14fbbee833
Contradictive88 <lanceorville5@gmail.com>
2024-05-29 18:57:33 +0800

    set /login for auth controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/login', [AuthController::class, 'login']);
