106412a5faad5f716152bdbe2233ab8316483284
Contradictive88 <lanceorville5@gmail.com>
2024-05-30 16:02:47 +0800

    added jwt.auth to /logout in routes

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::middleware(['jwt.auth'])->post('/logout', [AuthController::class, 'logout']);
