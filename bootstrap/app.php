1cc443c3a818f799400aa29c633d3df18d628916
Contradictive88 <lanceorville5@gmail.com>
2024-06-08 21:05:34 +0800

    removed cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\Cors;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(Cors::class);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
