823b9971270ad9d798e5d6237fe760d49237ab7d
lancedalanon <lanceorville5@gmail.com>
2024-09-09 21:24:34 +0800

    Create attendance controller

---
+++ app/Http/Controllers/v1/CompanyAdmin/AttendanceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\v1\CompanyAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AttendanceController extends Controller
+{
+    //
+}
