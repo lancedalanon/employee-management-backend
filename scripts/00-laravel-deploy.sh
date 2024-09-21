7abb50f8f2e0f212e9cf9f2972233db0a9e0ba41
lancedalanon <lanceorville5@gmail.com>
2024-09-21 10:15:24 +0800

    Add cloudinary provider

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,6 +2,9 @@
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
+echo "Publishing cloudinary provider..."
+php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider" --tag="cloudinary-laravel-config"
+
 echo "Optimize clear"
 php artisan optimize:clear
 
