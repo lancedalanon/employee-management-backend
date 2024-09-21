6fee20c2ae5545b745bb96c572f0bb7aaf665605
lancedalanon <lanceorville5@gmail.com>
2024-09-21 17:33:31 +0800

    Add /ping as a route for api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/ping', function () {
+    return response()->json(['message' => 'Server is up.'], 200);
+})->name('ping');
+
 Route::prefix('v1')->name('v1.')->namespace('Api\V1')->group(function () {
     require __DIR__ . '/api/v1/admin.php';
     require __DIR__ . '/api/v1/company_admin.php';
